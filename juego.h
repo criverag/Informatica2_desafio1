08623968df62a4820f4ad980dea394370cc4cefb
criverag <cristian.rivera0@soy.sena.edu.co>
2026-03-16 22:30:53 -0500

    Archivo juego.h actualizado y agregado.

---
+++ juego.h
@@ -0,0 +1,22 @@
+// juego.h
+// Archivo de cabecera principal del proyecto.
+// Aqui se incluyen las librerias y se declaran los prototipos
+// de las funciones que se van a usar en el programa.
+//
+// Por ahora solo tengo las funciones del menu porque es lo
+// primero que quise implementar para ver como quedaria
+// la estructura del programa.
+
+
+#include <iostream>
+
+using namespace std;
+
+// Prototipos de las funciones del menu
+// Las defino aqui para que main.cpp las pueda usar
+// aunque esten escritas en menu.cpp
+void mostrarMenu();
+void mostrarInstrucciones();
+void iniciarJuego();
+
+
